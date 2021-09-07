29a9793b79a660522987cf9a8ce911456945421d
b84192223 <busra.gungor1@huawei.com>
2021-09-07 10:20:12 +0300

    viewmodel add

--- app/src/main/java/com/example/databindingbg/MainActivity.kt
+++ app/src/main/java/com/example/databindingbg/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.databindingbg.data.Student
 import com.example.databindingbg.databinding.ActivityMainBinding
+import com.example.databindingbg.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
 
         binding.student = getStudent()
 
@@ -24,6 +28,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.loadingBtn.setOnClickListener {
             startLoading()
+        }
+
+        binding.countText.text = viewModel.getCurrentCount().toString()
+        binding.clickHereBtn.setOnClickListener {
+            binding.countText.text = viewModel.getUpdatedCount().toString()
         }
 
     }
